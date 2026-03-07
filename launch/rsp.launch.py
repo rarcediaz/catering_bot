aa8d819377585d18072bffc30f594f3572385c8e
Rodrigo Arce Diaz <rarce2002@gmail.com>
2026-03-07 00:35:37 -0800

    Added YDlidar config file and node to launch file

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -12,16 +12,21 @@ import xacro
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    pkg_path = get_package_share_directory('my_bot')
+
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file).toxml()
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+
+    # ----------------------------
+    # Robot State Publisher
+    # ----------------------------
+    params = {
+        'robot_description': robot_description_config,
+        'use_sim_time': use_sim_time
+    }
+
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -29,13 +34,36 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # ----------------------------
+    # YDLIDAR parameters
+    # ----------------------------
+    lidar_params = os.path.join(
+        pkg_path,
+        'config',
+        'ydlidar.yaml'
+    )
 
-    # Launch!
+    # ----------------------------
+    # YDLIDAR Node
+    # ----------------------------
+    ydlidar_node = Node(
+        package='ydlidar_ros2_driver',
+        executable='ydlidar_ros2_driver_node',
+        parameters=[lidar_params],
+        output='screen'
+    )
+
+    # ----------------------------
+    # Launch everything
+    # ----------------------------
     return LaunchDescription([
+
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
+            description='Use sim time if true'
+        ),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        ydlidar_node
     ])
