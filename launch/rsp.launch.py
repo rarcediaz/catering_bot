dfdd05a47ede30a902abb484dec99562c8fd6c63
Rodrigo Arce Diaz <rarce2002@gmail.com>
2026-03-07 01:51:15 -0800

    Added temp joint state publisher to see working robot in rviz

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -66,6 +66,17 @@ def generate_launch_description():
     )
 
     # ----------------------------
+    # Temp joint state publisher so we can see wheels
+    # ----------------------------
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': False}]
+    )
+
+    # ----------------------------
     # Launch everything
     # ----------------------------
     return LaunchDescription([
@@ -76,6 +87,7 @@ def generate_launch_description():
         ),
 
         node_robot_state_publisher,
+        joint_state_publisher_node,
         ydlidar_node,
         static_tf_node
     ])
