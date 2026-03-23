8a9b2c248d1a41e75f1f60cab2476e26e81d71b8
Rodrigo Arce Diaz <rarce2002@gmail.com>
2026-03-23 14:24:39 -0700

    Changes

--- launch/ydlidar.launch.py
+++ launch/ydlidar.launch.py
@@ -1,43 +1,23 @@
-import os
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
+    package_name = 'my_bot'
+    ydlidar_params = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'ydlidar.yaml'
+    )
 
     ydlidar_node = Node(
         package='ydlidar_ros2_driver',
         executable='ydlidar_ros2_driver_node',
         name='ydlidar',
         output='screen',
-        parameters=[{
-            'port': '/dev/ttyUSB0',
-            'frame_id': 'laser_frame',
-
-            'baudrate': 115200,
-            'lidar_type': 1,
-            'device_type': 0,
-
-            'isSingleChannel': True,
-            'intensity': False,
-
-            'sample_rate': 5,
-            'abnormal_check_count': 4,
-            'fixed_resolution': True,
-
-            'reversion': False,
-            'inverted': False,
-            'auto_reconnect': True,
-
-            'angle_min': -180.0,
-            'angle_max': 180.0,
-
-            'range_min': 0.1,
-            'range_max': 12.0,
-
-            'frequency': 7.0
-        }]
+        parameters=[ydlidar_params]
     )
 
     return LaunchDescription([
