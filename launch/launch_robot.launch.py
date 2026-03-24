df854029285ed1ad683fd807499298567644935d
Rodrigo Arce Diaz <rarce2002@gmail.com>
2026-03-24 13:00:52 -0700

    Added controller support remote

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -3,9 +3,11 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction, RegisterEventHandler
+from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessStart
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
@@ -13,6 +15,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     package_name = 'my_bot'
+    use_joystick = LaunchConfiguration('use_joystick')
 
     # Robot State Publisher
     rsp = IncludeLaunchDescription(
@@ -35,7 +38,8 @@ def generate_launch_description():
     joystick = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )])
+                )]),
+                condition=IfCondition(use_joystick)
     )
 
 
@@ -115,6 +119,11 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_joystick',
+            default_value='true',
+            description='Launch local joystick teleop on this machine if true.'
+        ),
         rsp,
         joystick,
         ydlidar,
