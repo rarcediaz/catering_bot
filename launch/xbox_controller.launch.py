a4a6e98df3caeb150451d03107dc221abde41ba1
Rodrigo Arce Diaz <rarce2002@gmail.com>
2026-04-03 15:07:31 -0700

    Changes to launch files

--- launch/xbox_controller.launch.py
+++ launch/xbox_controller.launch.py
@@ -1,21 +1,11 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    package_name = 'my_bot'
-    xbox_script = os.path.join(
-        get_package_share_directory(package_name),
-        'launch',
-        'xbox_controller.py'
-    )
-
-    xbox_controller = ExecuteProcess(
-        cmd=['python3', xbox_script],
+    xbox_controller = Node(
+        package='my_bot',
+        executable='xbox_controller.py',
         output='screen',
     )
 
